891ed1cc0491599e4eb7ece3a8a25b6c96f710eb
Paulo Victor <paulo.v.prado@hotmail.com>
2022-06-30 17:06:50 -0300

    requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,16 @@
 const data = require('../data/zoo_data');
 
+const colaboradores = data.employees;
+
 function isManager(id) {
-  // seu código aqui
+  return colaboradores.some((e) => e.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const f = colaboradores.filter((e) => e.managers.includes(managerId));
+  const arr = f.map(({ firstName, lastName }) => `${firstName} ${lastName}`);
+  if (isManager(managerId) === true) return arr;
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
