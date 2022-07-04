c895d6e6f202cd659886f9b5ed25c317135ed6fa
Paulo Victor <paulo.v.prado@hotmail.com>
2022-07-04 20:39:18 -0300

    requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,28 @@
+/* eslint-disable no-shadow */
 const data = require('../data/zoo_data');
 
+const valores = data.prices;
+//  console.log(valores);
+const { adult, senior, child } = valores;
+// console.log(adult);
+// console.log(senior);
+// console.log(child);
+
 function countEntrants(entrants) {
-  // seu código aqui
+  const visitantes = {
+    child: entrants.filter((e) => e.age < 18).length,
+    adult: entrants.filter((e) => e.age >= 18 && e.age < 50).length,
+    senior: entrants.filter((e) => e.age >= 50).length,
+  };
+  return visitantes;
 }
 
 function calculateEntry(entrants) {
-  // seu código aquii
+  if (typeof entrants === 'undefined' || Object.keys(entrants).length === 0) { return 0; }
+  const visitors = countEntrants(entrants);
+  const sum = child * visitors.child
+   + adult * visitors.adult + senior * visitors.senior;
+  return sum;
 }
 
 module.exports = { calculateEntry, countEntrants };
