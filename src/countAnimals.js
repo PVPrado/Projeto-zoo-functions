acc638110ea0831fe0899aeea1207c8be2d2fc8c
Paulo Victor <paulo.v.prado@hotmail.com>
2022-07-04 18:50:00 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+const { species } = data;
+
+function countAnimals(animal, age) {
+  const quantAnimais = {};
+  if (!animal) {
+    species.forEach((element) => {
+      quantAnimais[element.name] = element.residents.length;
+    });
+    // console.log(quantAnimais);
+    return quantAnimais;
+  }
+  const { specie } = animal;
+  const filtered = species.filter((el) => el.name === specie);
+  const [animalEscolhido] = filtered;
+  if (animal.sex === undefined) { return animalEscolhido.residents.length; }
+  return animalEscolhido.residents.filter((residente) => residente.sex === animal.sex).length;
 }
+
+countAnimals();
 
 module.exports = countAnimals;
