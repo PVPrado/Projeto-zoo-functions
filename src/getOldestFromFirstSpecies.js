d608c56db09150108f7beecfebd1f5bcf5e1fed0
Paulo Victor <paulo.v.prado@hotmail.com>
2022-07-04 22:52:50 -0300

    requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+// console.log(species);
+const { employees } = data;
+// console.log(employees);
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const colaborador = employees.find((pessoa) => pessoa.id === id);
+  const animal = colaborador.responsibleFor[0];
+  const ani = species.find((e) => e.id === animal);
+  let age = 0;
+  let resultado = [];
+  ani.residents.forEach((el) => {
+    if (el.age > age) {
+      age = el.age;
+      resultado = [el.name, el.sex, el.age];
+    }
+  });
+  return resultado;
 }
 
 module.exports = getOldestFromFirstSpecies;
