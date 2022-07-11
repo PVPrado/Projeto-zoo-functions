384a440614c5472ebb662adeb738dbd99d7cfb61
Paulo Victor <paulo.v.prado@hotmail.com>
2022-07-11 06:55:15 -0300

    requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,50 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { employees, species } = data;
+function geraListaCompleta() {
+  const listaEmpregados = [];
+  employees.forEach((employee) => {
+    listaEmpregados.push(
+      {
+        id: employee.id,
+        fullName: `${employee.firstName} ${employee.lastName}`,
+        species: (employee.responsibleFor).map((item) =>
+          species.find((specie) => specie.id === item).name),
+        locations: (employee.responsibleFor).map((item) =>
+          species.find((specie) => specie.id === item).location),
+      },
+    );
+  });
+  return listaEmpregados;
+}
+function capturaEmpregado(input) {
+  const empregado = {
+    id: input.id,
+    fullName: `${input.firstName} ${input.lastName}`,
+    species: input.responsibleFor.map((item) =>
+      species.find((specie) => specie.id === item).name),
+    locations: input.responsibleFor.map((item) =>
+      species.find((specie) => specie.id === item).location),
+  };
+  return empregado;
+}
+function getEmployeesCoverage(param) {
+  if (!param) return geraListaCompleta();
+  let employeeCoverage = {};
+  let entradaValida = false;
+  employeeCoverage = employees.find((employee) => employee.id === param.id);
+  if (employeeCoverage !== undefined) {
+    return capturaEmpregado(employeeCoverage);
+  }
+  employeeCoverage = employees.find((employee) =>
+    ((employee.firstName === param.name) || (employee.lastName === param.name)));
+  if (employeeCoverage !== undefined) {
+    entradaValida = true;
+    return capturaEmpregado(employeeCoverage);
+  }
+  if (entradaValida === false) {
+    throw new Error('Informações inválidas');
+  }
 }
 
 module.exports = getEmployeesCoverage;
