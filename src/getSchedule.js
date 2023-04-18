54897cf40cd1faefe9fa9e0410d0e4582bd8c000
Paulo Victor <paulo.v.prado@hotmail.com>
2023-04-18 19:26:13 -0300

    getSchedule

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+const { hours } = data;
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  const animalName = species.filter((animal) => animal.name === scheduleTarget);
+  const diaDisponivel = animalName.find((el) => el.availability).availability;
+  // console.log(diaDisponivel);
 }
+
+getSchedule('lions');
 
 module.exports = getSchedule;
